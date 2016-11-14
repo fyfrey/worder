3c929bf2ea85a7b8ce9b69d53da72ff54562fa71
Fynn Schröder <zody22@gmail.com>
2016-11-14 23:35:28 +0100

    optimized inner loop (faster as server, slower as command line utility)

--- source/app.d
+++ source/app.d
@@ -4,6 +4,7 @@ import std.conv : to;
 import std.experimental.allocator: theAllocator, allocatorObject, makeArray, dispose;
 import std.experimental.allocator.mallocator : Mallocator;
 import std.string : lineSplitter;
+import std.algorithm : sort, SwapStrategy;
 
 import containers.dynamicarray : DynamicArray;
 
@@ -30,41 +31,56 @@ int main(string[] args)
 		words ~= word;
 	}
 
+	sort!((a,b) => a.length < b.length, SwapStrategy.stable)(words[]);
+
+	size_t[] indexes = theAllocator.makeArray!size_t(words[$-1].length + 1);
+	scope(exit) theAllocator.dispose(indexes);
+
+	indexes[$-1] = words.length;
+	size_t l;
+
+	foreach(i, word; words) {
+		if(word.length > l) {
+			indexes[l] = i;
+			l = word.length;
+		}
+	}
+
 	char[] letters = makeArray!char(theAllocator, args[1].length);
 	scope(exit) dispose(theAllocator, letters);
 	letters[] = args[1];
 	letters.toLowerInPlace;
 
 	writeln("Solutions:");
-	foreach (result; solveWord(words, letters, to!uint(args[2]))) writeln(result);
-	
+	uint size = to!uint(args[2]);
+	auto slice = words[indexes[size-1]..indexes[size]];
+	DynamicArray!(char[], Mallocator, false) results;
+	solveWord(slice, letters, results);
+
+	foreach (result; results) writeln(result);
+
 	return 0;
 }
 
-private auto solveWord(R)(ref R words, const(char)[] letters, uint size)
+private void solveWord(R, O)(ref R words, const(char)[] letters, ref O outputRange) @nogc nothrow
 {
 	ubyte[256] masterTable;
 	foreach(ubyte b; letters) masterTable[b]++;
 	ubyte[256] tempTable = void;
 
-	DynamicArray!(char[], Mallocator, false) results;
-
 	loop1: foreach (word; words)
 	{
-		if (word.length != size) continue;
 		tempTable = masterTable;
 		foreach(ubyte c; word)
 		{
 			if (!tempTable[c]) continue loop1;
 			tempTable[c]--;
 		}
-		results ~= word;
+		outputRange ~= word;
 	}
-
-	return results;
 }
 
-private bool checkArgs(string[] args)
+private bool checkArgs(string[] args) nothrow
 {
 	if (args.length < 3) return false;
 	try
